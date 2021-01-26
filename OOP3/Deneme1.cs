9f4f810cc5fb148051a2a7bae7a2c403b456724a
Ahmet Kocadag <kocadag80@hotmail.com>
2021-01-27 02:12:37 +0300

    Add project files.

---
+++ OOP3/Deneme1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class Deneme1:ILoggerService
+    {
+        public void Log()
+        {
+            Console.WriteLine("Denedim");
+        }
+
+        
+    }
+}
